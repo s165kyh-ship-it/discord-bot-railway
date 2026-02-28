c4b531364468929641b0b0d7b49c821869af02fc
s165kyh-ship-it <s165k.yh@gmail.com>
2026-02-28 21:29:36 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}! Bot is online.')
+
+bot.run(os.environ['DISCORD_TOKEN'])
